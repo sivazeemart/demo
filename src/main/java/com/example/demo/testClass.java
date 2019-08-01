4cd2d8af93b5cb3ebf839207ce5b27701975fbf7
sivaprakash <sivaprakash@zeemart.asia>
2019-08-01 16:26:05 +0530

    first commit

---
+++ src/main/java/com/example/demo/testClass.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import com.tutorial.starterUtil.service.TestSample;
+
+public class testClass implements TestSample {
+
+    @Override
+    public void startTest() {
+        // TODO Auto-generated method stub
+        System.out.println("Custom");
+    }
+
+}
